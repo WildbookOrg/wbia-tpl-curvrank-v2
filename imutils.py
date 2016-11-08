e1d28ca76e8670d327a0da56ec5a597677dca1e2
Hendrik Weideman <weideh@rpi.edu>
2016-11-08 10:15:54 -0500

    Removed unused function.

--- imutils.py
+++ imutils.py
@@ -24,28 +24,6 @@ def center_pad_with_transform(img, imsize):
     return resz, M
 
 
-def center_pad(img, length):
-    old_height, old_width = img.shape[:2]
-    scale = (1. * length) / max(old_height, old_width)
-    rsz = cv2.resize(
-        img, dsize=(0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
-    new_height, new_width = rsz.shape[:2]
-    if new_height > new_width:
-        padded = np.zeros((length, length, 3), dtype=np.uint8)
-        start = int(np.round((length - new_width) / 2.))
-        end = start + new_width
-        padded[:, start:end] = rsz
-        return padded
-    elif new_height < new_width:
-        padded = np.zeros((length, length, 3), dtype=np.uint8)
-        start = int(np.round((length - new_height) / 2.))
-        end = start + new_height
-        padded[start:end, :] = rsz
-        return padded
-    else:
-        return rsz
-
-
 def test_center_pad_with_transform():
     img = np.full((384, 512), 255, dtype=np.uint8)
     resz, M = center_pad_with_transform(img, 256)
