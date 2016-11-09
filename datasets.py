6011cecbdcb85fd12d1c52f3794cc9230eda1fdf
Hendrik Weideman <weideh@rpi.edu>
2016-11-09 18:05:30 -0500

    Utility function to separate NZ dataset into queries/database.

--- datasets.py
+++ datasets.py
@@ -1,9 +1,9 @@
 import cv2
+import numpy as np
 import sqlite3
+from collections import defaultdict
 from os import listdir
-from os.path import join
-
-import os
+from os.path import basename, join, splitext
 
 
 def load_dataset(name):
@@ -79,3 +79,57 @@ def load_sdrp_dataset():
     cur.close()
 
     return data_list
+
+
+def separate_database_queries(name, fpath_list, ind_list, enc_list, curv_dict):
+    if name == 'nz':
+        return separate_nz_dataset(fpath_list, ind_list, enc_list, curv_dict)
+    elif name == 'sdrp':
+        return separate_sdrp_dataset()
+    else:
+        assert False, 'bad dataset name: %s' % (name)
+
+
+def separate_nz_dataset(fpath_list, ind_list, enc_list, curv_dict):
+    # stores all encounters in which an individual appears
+    ind_enc_dict = defaultdict(set)
+    # stores all the curvature vectors for an encounter
+    enc_curv_dict = defaultdict(list)
+    for fpath, ind, enc in zip(fpath_list, ind_list, enc_list):
+        fname = splitext(basename(fpath))[0]
+        if fname in curv_dict.keys():
+            ind_enc_dict[ind].add(enc)
+            enc_curv_dict[enc].append(curv_dict[fname])
+
+    # db_dict: {'i1': [v1, v2, ..., vn]}
+    # qr_dict: {'i2': {'e1': [v1, v2, ..., vm]}}
+    db_dict, qr_dict = {}, {}
+    individuals = ind_enc_dict.keys()
+    for ind in individuals:
+        encounters = ind_enc_dict[ind]
+        num_encounters = len(encounters)
+        if num_encounters > 1:
+            # get number of curvature vectors appearing in each encounter
+            num_curvs = [len(enc_curv_dict[e]) for e in encounters]
+            max_idx = np.argmax(num_curvs)
+            qr_enc_dict = {}
+            for idx, enc in enumerate(encounters):
+                if idx == max_idx:
+                    db_dict[ind] = enc_curv_dict[enc]
+                else:
+                    qr_enc_dict[enc] = enc_curv_dict[enc]
+
+            qr_dict[ind] = qr_enc_dict
+        else:
+            print('individual %s has only %d encounters' % (
+                ind, num_encounters))
+
+    # we only use individuals with at least two encounters
+    for qind in qr_dict.keys():
+        assert qind in db_dict.keys(), '%s missing from db!' % (qind)
+
+    return db_dict, qr_dict
+
+
+def separate_sdrp_dataset():
+    return None
