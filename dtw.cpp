1450ff042e25c95d8790730f0a2dc10b151b2f37
Hendrik Weideman <weideh@rpi.edu>
2017-02-13 15:25:35 -0500

    New C++ implementations of cost functions.

--- dtw.cpp
+++ dtw.cpp
@@ -1,48 +1,60 @@
-#include <cstdlib>
-#include <cstdio>
-#include <cmath>
-#include <cfloat>
-//#include <stdint.h>
-#include <ctime>
-//#include <cstdint>
-#include <Eigen/Dense>
 #include <iostream>
-//#include <cblas>
-// this is bad don't do this kids
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#include <algorithm>
+#include <Eigen/Dense>
+#include <math.h>
 
-//#define DLLEXPORT extern "C" __declspec(dllexport)
+typedef Eigen::Array<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> ArrayType;
+typedef Eigen::Map<ArrayType> MapType;
 
-using namespace Eigen;
+extern "C" void weighted_chi_square(float* x1, float* x2, float* w,
+                                    int m, int n, int window,
+                                    float* costs_out) {
+  MapType costs((float*) costs_out, m, m);
 
-typedef Matrix<float, Dynamic, Dynamic, RowMajor> NDArrayFlattened;
-typedef Map<NDArrayFlattened> ExternNDArrayf;
+  MapType X1((float*) x1, m, n);
+  MapType X2((float*) x2, m, n);
+  MapType W((float*) w, m, n);
 
-typedef Matrix<int, Dynamic, Dynamic, RowMajor> NDArrayFlattenedi;
-typedef Map<NDArrayFlattenedi> ExternNDArrayi;
+  ArrayType W1 = (X1 - W).abs();
+  ArrayType W2 = (X2 - W).abs();
+  ArrayType xi, wi;
+  ArrayType xj, wj;
 
-extern "C" void dtw_curvweighted(float * seq1curvv, float * seq2curvv,
-                                 int seq1_len, int seq2_len, int window,
-                                 int curv_hist_size, float * curv_hist_weightsv,
-                                 float * distmat_outv) {
+  float cost;
+  for (int i = 1; i < m; ++i) {
+    for (int j = std::max(1, i - window); j < std::min(m, i + window); ++j) {
+      xi = X1.row(i);
+      xj = X2.row(j);
+      wi = W1.row(i);
+      wj = W2.row(j);
 
-    window = MAX(window, abs(seq1_len - seq2_len) + 1);
-    ExternNDArrayf distmat_out((float*)distmat_outv, seq1_len, seq2_len);
-    ExternNDArrayf seq1curv((float*)seq1curvv, seq1_len, curv_hist_size);
-    ExternNDArrayf seq2curv((float*)seq2curvv, seq2_len, curv_hist_size);
-
-
-    ExternNDArrayf curv_hist_weights((float*)curv_hist_weightsv, curv_hist_size, 1);
-
-    float dist;
-    for (int i = 1; i < seq1_len; i++) {
-        for (int j = MAX(1, i - window); j < MIN(seq2_len, i + window); j++) {
-            dist = ((seq1curv.row(i).array() * curv_hist_weights.transpose().array()) - 
-                    (seq2curv.row(j).array() * curv_hist_weights.transpose().array())).matrix().norm();
-            distmat_out(i,j) = dist + MIN(distmat_out(i, j-1), 
-                                        MIN(distmat_out(i-1, j),
-                                            distmat_out(i-1, j-1)));
-        }
+      cost = ((xi - xj) * (xi - xj) / (wi + wj + 1e-6)).sum();
+      costs(i, j) = cost + std::min(costs(i, j - 1),
+          std::min(costs(i - 1, j), costs(i - 1, j - 1)));
     }
-}
+  }
+} 
+
+extern "C" void euclidean(float* x1, float* x2, float* w,
+                          int m, int n, int window,
+                          float* costs_out) {
+  MapType costs((float*) costs_out, m, m);
+
+  MapType X1((float*) x1, m, n);
+  MapType X2((float*) x2, m, n);
+  MapType weights((float*) w, 1, n);
+
+  ArrayType xi, xj;
+
+  float cost;
+  for (int i = 1; i < m; ++i) {
+    for (int j = std::max(1, i - window); j < std::min(m, i + window); ++j) {
+      xi = X1.row(i);
+      xj = X2.row(j);
+
+      cost = sqrt((weights * (xi - xj) * (xi - xj)).sum());
+      costs(i, j) = cost + std::min(costs(i, j - 1),
+          std::min(costs(i - 1, j), costs(i - 1, j - 1)));
+    }
+  }
+} 
