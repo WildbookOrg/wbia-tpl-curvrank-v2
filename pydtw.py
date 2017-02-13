986b82c3e42e8d366ec8e99e7c176f2d9a17bf3f
Hendrik Weideman <weideh@rpi.edu>
2017-02-13 15:25:59 -0500

    Wrapper code for new C++ implementations.

--- pydtw.py
+++ pydtw.py
@@ -1,42 +1,65 @@
 import numpy as np
 
 import ctypes
-lib = ctypes.cdll.LoadLibrary('dtw.so')
+
+
+costs_lib = ctypes.cdll.LoadLibrary('dtw.so')
 
 ndmat_f_type = np.ctypeslib.ndpointer(
     dtype=np.float32, ndim=2, flags='C_CONTIGUOUS')
 ndmat_i_type = np.ctypeslib.ndpointer(
     dtype=np.int32, ndim=2, flags='C_CONTIGUOUS')
 
-dtw_curvweighted = lib.dtw_curvweighted
-dtw_curvweighted.argtypes = [ndmat_f_type, ndmat_f_type,
-                             ctypes.c_int, ctypes.c_int,
-                             ctypes.c_int, ctypes.c_int,
-                             ndmat_f_type, ndmat_f_type]
+
+dtw_chi_square_cpp = costs_lib.weighted_chi_square
+dtw_euclidean_cpp  = costs_lib.euclidean
+
+dtw_chi_square_cpp.argtypes = [ndmat_f_type, ndmat_f_type, ndmat_f_type,
+                               ctypes.c_int, ctypes.c_int, ctypes.c_int,
+                               ndmat_f_type]
+dtw_euclidean_cpp.argtypes   = [ndmat_f_type, ndmat_f_type, ndmat_f_type,
+                                ctypes.c_int, ctypes.c_int, ctypes.c_int,
+                                ndmat_f_type]
 
 
-# https://github.com/zmjjmz/ibeis-flukematch-module/blob/b6bfd75cd45f8ad3177e34a809e1d0b739150772/flukematch_lib.cpp#L25
-def compute_dtw_fast(qcurv, dcurv, weights, window=50):
-    weights_nd = np.array(weights, dtype=np.float32).reshape(-1, 1)
-    weights_nd = np.ascontiguousarray(weights_nd)
+def dtw_euclidean(qcurv, dcurv, weights, window):
+    assert qcurv.dtype == np.float32, 'qcurv.dtype = %s' % qcurv.dtype
+    assert dcurv.dtype == np.float32, 'dcurv.dtype = %s' % dcurv.dtype
+    assert weights.dtype == np.float32, 'weights.dtype = %s' % weights.dtype
+    assert qcurv.flags.c_contiguous
+    assert dcurv.flags.c_contiguous
+    assert weights.flags.c_contiguous
+    assert qcurv.shape == dcurv.shape
+    assert qcurv.shape[1] == weights.shape[1]
+    assert qcurv.ndim == dcurv.ndim == weights.ndim == 2
 
-    if qcurv.dtype != np.float32:
-        qcurv_nd = np.ascontiguousarray(qcurv, dtype=np.float32)
-    else:
-        qcurv_nd = qcurv
-    if dcurv.dtype != np.float32:
-        db_curv_nd = np.ascontiguousarray(dcurv, dtype=np.float32)
-    else:
-        db_curv_nd = dcurv
+    m, n = qcurv.shape
+    costs_out = np.full((m, m), np.inf, dtype=np.float32)
+    costs_out[0, 0] = 0.
+    dtw_euclidean_cpp(
+        qcurv, dcurv, weights, m, n, window,
+        costs_out
+    )
 
-    dcurv_nd = dcurv
-    query_len = qcurv_nd.shape[0]
-    db_len = dcurv_nd.shape[0]
-    distance_mat = np.full((query_len, db_len), np.inf, dtype=np.float32)
-    distance_mat[0, 0] = 0
-    dtw_curvweighted(
-        qcurv_nd, db_curv_nd, query_len, db_len, window,
-        weights_nd.shape[0], weights_nd, distance_mat)
+    return costs_out[-1, -1]
 
-    distance = distance_mat[-1, -1]
-    return distance
+
+def dtw_chi_square(qcurv, dcurv, weights, window):
+    assert qcurv.dtype == np.float32, 'qcurv.dtype = %s' % qcurv.dtype
+    assert dcurv.dtype == np.float32, 'dcurv.dtype = %s' % dcurv.dtype
+    assert weights.dtype == np.float32, 'weights.dtype = %s' % weights.dtype
+    assert qcurv.flags.c_contiguous
+    assert dcurv.flags.c_contiguous
+    assert weights.flags.c_contiguous
+    assert qcurv.shape == dcurv.shape == weights.shape
+    assert qcurv.ndim == dcurv.ndim == weights.ndim == 2
+
+    m, n = qcurv.shape
+    costs_out = np.full((m, m), np.inf, dtype=np.float32)
+    costs_out[0, 0] = 0.
+    dtw_chi_square_cpp(
+        qcurv, dcurv, weights, m, n, window,
+        costs_out
+    )
+
+    return costs_out[-1, -1]
