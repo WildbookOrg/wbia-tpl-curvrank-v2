cb6b7eda925dc2811740da21d6564ff91b8ef0a3
Hendrik Weideman <weideh@rpi.edu>
2017-02-20 08:57:42 -0500

    Implement spatial weighting in C++.

--- dtw.cpp
+++ dtw.cpp
@@ -13,22 +13,29 @@ extern "C" void weighted_chi_square(float* x1, float* x2, float* w,
 
   MapType X1((float*) x1, m, n);
   MapType X2((float*) x2, m, n);
-  MapType W((float*) w, m, n);
+  MapType weights((float*) w, m, 1);
 
-  ArrayType W1 = (X1 - W).abs();
-  ArrayType W2 = (X2 - W).abs();
-  ArrayType xi, wi;
-  ArrayType xj, wj;
+  // pre-computed offsets
+  ArrayType X1p = (X1 - 0.5).abs();
+  ArrayType X2p = (X2 - 0.5).abs();
+
+  ArrayType xi, xj, xip, xjp;
+  float wi, wj;
 
   float cost;
   for (int i = 1; i < m; ++i) {
     for (int j = std::max(1, i - window); j < std::min(m, i + window); ++j) {
+      // original points
       xi = X1.row(i);
       xj = X2.row(j);
-      wi = W1.row(i);
-      wj = W2.row(j);
+      // pre-computed offsets with abs. value
+      xip = X1p.row(i);
+      xjp = X2p.row(j);
+      // spatial weights
+      wi = weights(i, 0);
+      wj = weights(j, 0);
 
-      cost = ((xi - xj) * (xi - xj) / (wi + wj + 1e-6)).sum();
+      cost = wi * wj * ((xi - xj) * (xi - xj) / (xip + xjp + 1e-6)).sum();
       costs(i, j) = cost + std::min(costs(i, j - 1),
           std::min(costs(i - 1, j), costs(i - 1, j - 1)));
     }
@@ -53,6 +60,34 @@ extern "C" void euclidean(float* x1, float* x2, float* w,
       xj = X2.row(j);
 
       cost = sqrt((weights * (xi - xj) * (xi - xj)).sum());
+      costs(i, j) = cost + std::min(costs(i, j - 1),
+          std::min(costs(i - 1, j), costs(i - 1, j - 1)));
+    }
+  }
+}
+
+extern "C" void weighted_euclidean(float* x1, float* x2, float* w,
+                                   int m, int n, int window,
+                                   float* costs_out) {
+  MapType costs((float*) costs_out, m, m);
+
+  MapType X1((float*) x1, m, n);
+  MapType X2((float*) x2, m, n);
+  MapType weights((float*) w, m, 1);
+
+  ArrayType xi, xj;
+  float wi, wj;
+
+  float cost;
+  for (int i = 1; i < m; ++i) {
+    for (int j = std::max(1, i - window); j < std::min(m, i + window); ++j) {
+      xi = X1.row(i);
+      xj = X2.row(j);
+
+      wi = weights(i, 0);
+      wj = weights(j, 0);
+
+      cost = wi * wj * sqrt(((xi - xj) * (xi - xj)).sum());
       costs(i, j) = cost + std::min(costs(i, j - 1),
           std::min(costs(i - 1, j), costs(i - 1, j - 1)));
     }
--- pydtw.py
+++ pydtw.py
@@ -13,13 +13,25 @@ ndmat_i_type = np.ctypeslib.ndpointer(
 
 dtw_chi_square_cpp = costs_lib.weighted_chi_square
 dtw_euclidean_cpp  = costs_lib.euclidean
+dtw_weighted_euclidean_cpp = costs_lib.weighted_euclidean
 
-dtw_chi_square_cpp.argtypes = [ndmat_f_type, ndmat_f_type, ndmat_f_type,
-                               ctypes.c_int, ctypes.c_int, ctypes.c_int,
-                               ndmat_f_type]
-dtw_euclidean_cpp.argtypes   = [ndmat_f_type, ndmat_f_type, ndmat_f_type,
-                                ctypes.c_int, ctypes.c_int, ctypes.c_int,
-                                ndmat_f_type]
+dtw_chi_square_cpp.argtypes = [
+    ndmat_f_type, ndmat_f_type, ndmat_f_type,
+    ctypes.c_int, ctypes.c_int, ctypes.c_int,
+    ndmat_f_type
+]
+
+dtw_euclidean_cpp.argtypes   = [
+    ndmat_f_type, ndmat_f_type, ndmat_f_type,
+    ctypes.c_int, ctypes.c_int, ctypes.c_int,
+    ndmat_f_type
+]
+
+dtw_weighted_euclidean_cpp.argtypes = [
+    ndmat_f_type, ndmat_f_type, ndmat_f_type,
+    ctypes.c_int, ctypes.c_int, ctypes.c_int,
+    ndmat_f_type
+]
 
 
 def dtw_euclidean(qcurv, dcurv, weights, window):
@@ -44,20 +56,43 @@ def dtw_euclidean(qcurv, dcurv, weights, window):
     return costs_out[-1, -1]
 
 
-def dtw_chi_square(qcurv, dcurv, weights, window):
+def dtw_weighted_chi_square(qcurv, dcurv, weights, window):
     assert qcurv.dtype == np.float32, 'qcurv.dtype = %s' % qcurv.dtype
     assert dcurv.dtype == np.float32, 'dcurv.dtype = %s' % dcurv.dtype
     assert weights.dtype == np.float32, 'weights.dtype = %s' % weights.dtype
     assert qcurv.flags.c_contiguous
     assert dcurv.flags.c_contiguous
     assert weights.flags.c_contiguous
-    assert qcurv.shape == dcurv.shape == weights.shape
+    assert qcurv.shape == dcurv.shape
+    assert qcurv.shape[0] == weights.shape[0]
     assert qcurv.ndim == dcurv.ndim == weights.ndim == 2
 
     m, n = qcurv.shape
     costs_out = np.full((m, m), np.inf, dtype=np.float32)
     costs_out[0, 0] = 0.
     dtw_chi_square_cpp(
+        qcurv, dcurv, weights, m, n, window,
+        costs_out
+    )
+
+    return costs_out[-1, -1]
+
+
+def dtw_weighted_euclidean(qcurv, dcurv, weights, window):
+    assert qcurv.dtype == np.float32, 'qcurv.dtype = %s' % qcurv.dtype
+    assert dcurv.dtype == np.float32, 'dcurv.dtype = %s' % dcurv.dtype
+    assert weights.dtype == np.float32, 'weights.dtype = %s' % weights.dtype
+    assert qcurv.flags.c_contiguous
+    assert dcurv.flags.c_contiguous
+    assert weights.flags.c_contiguous
+    assert qcurv.shape == dcurv.shape
+    assert qcurv.shape[0] == weights.shape[0]
+    assert qcurv.ndim == dcurv.ndim == weights.ndim == 2
+
+    m, n = qcurv.shape
+    costs_out = np.full((m, m), np.inf, dtype=np.float32)
+    costs_out[0, 0] = 0.
+    dtw_weighted_euclidean_cpp(
         qcurv, dcurv, weights, m, n, window,
         costs_out
     )
