7868d87d12b645cceef8d0dbf32e14029b1834ee
Hendrik Weideman <weideh@rpi.edu>
2017-02-22 10:12:11 -0500

    Removed implementation of unweighted Euclidean time-warping.

--- dtw.cpp
+++ dtw.cpp
@@ -42,30 +42,6 @@ extern "C" void weighted_chi_square(float* x1, float* x2, float* w,
   }
 }
 
-extern "C" void euclidean(float* x1, float* x2, float* w,
-                          int m, int n, int window,
-                          float* costs_out) {
-  MapType costs((float*) costs_out, m, m);
-
-  MapType X1((float*) x1, m, n);
-  MapType X2((float*) x2, m, n);
-  MapType weights((float*) w, 1, n);
-
-  ArrayType xi, xj;
-
-  float cost;
-  for (int i = 1; i < m; ++i) {
-    for (int j = std::max(1, i - window); j < std::min(m, i + window); ++j) {
-      xi = X1.row(i);
-      xj = X2.row(j);
-
-      cost = sqrt((weights * (xi - xj) * (xi - xj)).sum());
-      costs(i, j) = cost + std::min(costs(i, j - 1),
-          std::min(costs(i - 1, j), costs(i - 1, j - 1)));
-    }
-  }
-}
-
 extern "C" void weighted_euclidean(float* x1, float* x2, float* w,
                                    int m, int n, int window,
                                    float* costs_out) {
