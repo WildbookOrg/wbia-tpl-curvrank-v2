Fix bug in A* search where floating point precision would lead to the occasional random contour.

dorsal_utils.py
@@ -101,7 +101,23 @@ def find_fluke_keypoints(X):
     return start, end
 
 
-def extract_outline(img, msk, segm, start, end, allow_diagonal):
+def dorsal_cost_func(grad, dist):
+    W = 1. / np.clip(grad * dist, 1e-5, 1.)
+
+    return W
+
+
+def fluke_cost_func(grad, dist):
+    norm = cv2.normalize(
+        grad * dist, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX
+    )
+
+    W = np.exp(5. * (1. - norm))
+
+    return W
+
+
+def extract_outline(img, msk, segm, cost_func, start, end, allow_diagonal):
     assert img.ndim == 3, 'img.dim = %d != 3' % (img.ndim)
     assert segm.ndim == 2, 'segm.ndim = %d != 2' % (segm.ndim)
 
@@ -141,13 +157,10 @@ def extract_outline(img, msk, segm, start, end, allow_diagonal):
         dist, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX
     )
 
-    norm = cv2.normalize(
-        grad_norm * dist, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX
-    )
-
-    W = np.exp(5. * (1. - norm))
+    W = cost_func(grad_norm, dist)
 
     outline = astar_path(W, start, end, allow_diagonal=allow_diagonal)
+    #outline = astar_path(W, end, start, allow_diagonal=allow_diagonal)
 
     return outline
 

workers.py
@@ -210,7 +210,7 @@ def find_keypoints(fpath, method,
 # input1_targets: refinement_targets
 # input2_targets: segmentation_targets
 # input3_targets: keypoints_targets
-def extract_outline(fpath, scale, allow_diagonal,
+def extract_outline(fpath, scale, allow_diagonal, cost_func,
                     input1_targets, input2_targets, input3_targets,
                     output_targets):
     coords_target = output_targets[fpath]['outline-coords']
@@ -236,7 +236,7 @@ def extract_outline(fpath, scale, allow_diagonal,
         # points are ij
         start_refn, end_refn = np.floor(points_refn[:, ::-1]).astype(np.int32)
         outline = dorsal_utils.extract_outline(
-            rfn, msk, segm, start_refn, end_refn, allow_diagonal
+            rfn, msk, segm, cost_func, start_refn, end_refn, allow_diagonal
         )
     else:
         outline = np.array([])
@@ -244,6 +244,8 @@ def extract_outline(fpath, scale, allow_diagonal,
     # TODO: what to write for failed extractions?
     if outline.shape[0] > 0:
         rfn[outline[:, 0], outline[:, 1]] = (255, 0, 0)
+        rfn[start_refn[0], start_refn[1]] = (0, 0, 255)
+        rfn[end_refn[0], end_refn[1]] = (0, 0, 255)
 
     _, visual_buf = cv2.imencode('.png', rfn)
     with coords_target.open('wb') as f1,\