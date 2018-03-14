8b3b136520b58ce9200b0b23107efd2a612d7d6a
Jason Parham <parham@wildme.org>
2018-03-14 15:15:37 -0700

    Updated version

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 
 # THIS FILE IS GENERATED FROM SETUP.PY
 version = '0.1.0.dev0'
-git_revision = '0cf2b601a65bd20c05f3f2b98827885733021ca5'
+git_revision = '1598d0d894526d17bcd689cac0e6487db088499a'
 full_version = '%(version)s.%(git_revision)s' % {
     'version': version,
     'git_revision': git_revision,
