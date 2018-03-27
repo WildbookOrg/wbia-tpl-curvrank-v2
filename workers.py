c8e1934b9c95504e9193aad16f768cf2b174e200
Hendrik Weideman <weideh@rpi.edu>
2018-03-27 10:12:02 -0400

    Do not return the Annoy index, messes with serialization of multiprocessing.

--- workers.py
+++ workers.py
@@ -525,7 +525,6 @@ def build_annoy_index(data, fpath):
         index.add_item(i, data[i])
     index.build(10)
     index.save(fpath)
-    return index
 
 
 def identify_encounter_descriptors(qind, qenc, db_names, scales, k,
