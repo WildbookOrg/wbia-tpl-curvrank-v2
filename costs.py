26c8307045b13f96b7f00efb2781e8806dce44c8
Jason Parham <bluemellophone@gmail.com>
2022-06-10 13:37:12 -0700

    Fix segfault

--- costs.py
+++ costs.py
@@ -14,4 +14,4 @@ def hyp_cost_func(fine, coarse):
     cost = coarse * fine
     cost = cv2.normalize(cost, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 
-    return 1.0 / np.clip(cost, 1e-5, 1.0)
+    return 1.0 / np.clip(cost, 1e-4, 1.0)
